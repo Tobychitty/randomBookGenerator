26aa942b79dccfefcf9e781cb43f0e366bd34857
tobychitty <tobycvo@gmail.com>
2020-11-11 16:00:54 +0000

    Added random book functionality

--- main.js
+++ main.js
@@ -1,2 +1,47 @@
-console.log('yo');
+const books = [
+    {
+        title: 'Atomic Habits',
+        author: 'James Clear'
+    },
+    {
+        title: '1984',
+        author: 'George Orwell'
+    },
+    {
+        title: 'User Friendly',
+        author: 'Cliff Kuang with Robert Fabricant'
+    },
+    {
+        title: 'Sapiens',
+        author: 'Yuval Noah Harari'
+    },
+    {
+        title: 'Humankind',
+        author: 'Rutger Bregman'
+    },
+    {
+        title: 'Algorithms to Live By',
+        author: 'James Clear'
+    },
+    {
+        title: 'How to Own the World',
+        author: 'Andrew Craig'
+    },
+    {
+        title: 'Life 3.0',
+        author: 'Max Tegmark'
+    },
+    {
+        title: 'The Richest Man in Babylon',
+        author: 'George S. Clason'
+    },
+]
+
+const randomBook = () => {
+    const bookIndex = Math.floor(Math.random() * books.length);
+    const message = `You should read ${books[bookIndex].title} by ${books[bookIndex].author}.`;
+    console.log(message);
+}
+
+randomBook();
 
